c302d2953806deaa95ce86db64df75aee6c22dfd
kevin <929118967@qq.com>
2017-06-29 19:36:56 +0800

    10、配置bundle工程的infoPlist文件路径 ![这里写图片描述](http://img.blog.csdn.net/20170629190534799?watermark/2/text/aHR0cDovL2Jsb2cuY3Nkbi5uZXQvejkyOTExODk2Nw==/font/5a6L5L2T/fontsize/400/fill/I0JBQkFCMA==/dissolve/70/gravity/SouthEast)
    KNStaticBundle/KNStaticBundle-Info.plist
    
    11、 静态库工程配置
    other linker flags   -ObjC   -all_load
    
    12、 静态库工程的bitcode 关闭。 app工程的bitcode 进行关闭。

--- KNAPP/ViewController.m
+++ KNAPP/ViewController.m
@@ -8,21 +8,26 @@
 
 #import "ViewController.h"
 
+#import "KNTestWebViewController.h"
+#import "HCPEnvrionmentalVariables.h"
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    //    IPOpenShopMainViewController *openShopMainViewController =[[IPOpenShopMainViewController alloc]init];
+    
+    
+    [[HCPEnvrionmentalVariables shareEnvrionmentalVariables] setEnvrionmentalVariables:ENVRIONMENTAL_VARIABLES_UAT];
+    
+    
+    KNTestWebViewController *vc = [[KNTestWebViewController alloc]init];
+    
+    [self presentViewController:vc animated:YES completion:^{
+        
+    }];
 }
 
 
