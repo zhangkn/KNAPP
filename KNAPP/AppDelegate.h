0f9299d60f1787c599e66ca8ea5e410d85e7730c
kevin <929118967@qq.com>
2017-06-29 19:18:38 +0800

    Initial Commit

---
+++ KNAPP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KNAPP
+//
+//  Created by devzkn on 29/06/2017.
+//  Copyright © 2017 hisun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KNAPP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KNAPP
+//
+//  Created by devzkn on 29/06/2017.
+//  Copyright © 2017 hisun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
